534b3eabe86f157130355344c9e02582b4bb3075
Leandro <leandrolupano@hotmail.com>
2023-03-07 10:41:01 +0100

    🎉 Initial commit - React-Redux-Vite

---
+++ src/App.test.tsx
@@ -0,0 +1,12 @@
+import App from "./App";
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+
+describe("Given", () => {
+  describe("When", () => {
+    test("Then", () => {
+      render(<App></App>);
+      expect(screen.getByRole("heading")).toBeInTheDocument();
+    });
+  });
+});
